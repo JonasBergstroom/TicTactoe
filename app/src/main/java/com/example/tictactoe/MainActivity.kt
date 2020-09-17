f87d68b8c656d40a6e59c0f7bc04b891f7568975
jonasbergstrom <jontrian97@gmail.com>
2020-09-17 17:29:44 +0200

    Made a function for each buttons clicked and tested it.

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tictactoe
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -35,11 +36,54 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        Toast.makeText(this, "butt ID: "+buttID, Toast.LENGTH_LONG).show()
+        playGame(buttID,buttSelected)
 
+        // Making the "playGame" function connect to the "buttClick" function.
+
+    }
+
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
+
+    var activePlayer = 1
+
+
+    private fun playGame(buttID: Int, buttSelected: Button)
+
+    // Creates player 1 and player 2 in a ArrayList, and a function called "playGame".
+    // Also creates a "activePlayer" variable.
+    // The value of the activePlayer always start with "1", when the game starts.
+
+    {
+
+        if (activePlayer == 1) {
+            buttSelected.text = "X"
+            buttSelected.setBackgroundColor(Color.parseColor("#009193"))
+            player1.add(buttID)
+            activePlayer = 2
+
+            // player 1 starts with the activePlayers value as "1", until she/he chooses to click on a button.
+            // Then the activePlayer value becomes "2".
+            // When player 1 chooses a button to click, the appearance of the button change to an "X" with the background color blue.
+
+        }else{
+            buttSelected.text = "O"
+            player2.add(buttID)
+            buttSelected.setBackgroundColor(Color.parseColor("#FF9300"))
+            activePlayer = 1
+
+            // It is now player 2 turn to play because the value of the activePlayer became "2".
+            // After player 2 chooses to click on a button the activePlayer value becomes "1" again.
+            // When player 2 chooses a button to click, the appearance of the button change to an "O" with the background color orange.
+
+        }
+        buttSelected.isEnabled = false
+
+        // When a player chooses to click on a button, that button can't be clicked again.
+
+      }
 
     }
 
 
 
-}
