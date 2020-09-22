710cbcb9cfe33e42fecb14494c70be814d3b2ce2
jonasbergstrom <jontrian97@gmail.com>
2020-09-22 17:07:08 +0200

    Made the "welcome activity" show at the start of the app and made the "Play!" button work.

--- app/src/main/java/com/example/tictactoe/welcome.kt
+++ app/src/main/java/com/example/tictactoe/welcome.kt
@@ -1,11 +1,24 @@
 package com.example.tictactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class welcome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val button : Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity :: class.java)
+
+            startActivity(intent)
+        }
+
+        // When you click on the button "Play!" you will start the "MainActivity" and open the "playGame" tab.
     }
+
+
 }
